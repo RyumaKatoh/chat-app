76c6f2463746fa5c3706d487111d975a4fe02e45
RyumaKatoh <ryuma012645@gmail.com>
2023-07-23 19:29:17 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   devise_for :users
   resources :rooms, only: [:new, :create]
