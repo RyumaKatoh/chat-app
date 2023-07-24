4e69477d8f38c833935682f3882b2e73a00bb853
RyumaKatoh <ryuma012645@gmail.com>
2023-07-24 13:44:04 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: "messages#index"
   resources :users, only: [:edit, :update]
   devise_for :users
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
